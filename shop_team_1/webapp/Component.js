9d50677d2b4c1714c2aa1bb756af73377f6deffb
TIENBMW <16.vantien@gmail.com>
2024-08-06 15:00:33 +0000

    create page product and show all products

--- shop_team_1/webapp/Component.js
+++ shop_team_1/webapp/Component.js
@@ -5,9 +5,10 @@
 sap.ui.define([
         "sap/ui/core/UIComponent",
         "sap/ui/Device",
-        "shopteam1/model/models"
+        "shopteam1/model/models",
+        'sap/ui/model/json/JSONModel'
     ],
-    function (UIComponent, Device, models) {
+    function (UIComponent, Device, models,JSONModel) {
         "use strict";
 
         return UIComponent.extend("shopteam1.Component", {
@@ -15,20 +16,16 @@ sap.ui.define([
                 manifest: "json"
             },
 
-            /**
-             * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
-             * @public
-             * @override
-             */
             init: function () {
-                // call the base component's init function
+                var oProductsModel;
+    
                 UIComponent.prototype.init.apply(this, arguments);
-
-                // enable routing
-                this.getRouter().initialize();
-
-                // set the device model
-                this.setModel(models.createDeviceModel(), "device");
+    
+                // set products demo model on this sample
+                oProductsModel = new JSONModel(sap.ui.require.toUrl('shopteam1/mock') + '/products.json');
+                oProductsModel.setSizeLimit(1000);
+                this.setModel(oProductsModel, 'products');
+                console.log(oProductsModel)
             }
         });
     }
